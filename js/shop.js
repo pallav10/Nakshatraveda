a4bd6a3589650653c2e3cb3d820be20430445044
Purander Dua <puranderdua8@gmail.com>
2017-07-20 15:12:18 +0530

    added social media icons and new images

--- js/shop.js
+++ js/shop.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
 																				 	</div>
 																				 	<div class='col-md-6 col-sm-12 col-xs-12'>
 																				 		<p>`+data[i].description+`</p>
+																				 		<button id="load-desc" class="btn btn-default">Read More</button>
 																				 		<div class="row">
 																				 			<div class="col-md-4 col-sm-6 col-xs-6"><p data-price="`+data[i].price+`" class="item-price">INR `+data[i].price+`/- </p></div>
 																						 	<div class="col-md-4 col-sm-6 col-xs-6"><button data-price="`+data[i].price+`" id="buy-item" data-id="`+data[i].id+`" class="btn btn-default buy-items">Add to Cart</button></div>
@@ -98,6 +99,7 @@ $(document).ready(function() {
 																				 	</div>
 																				 	<div class='col-md-6 col-xs-12'>
 																				 		<p>`+data[i].description+`</p>
+																				 		<button id="load-desc" class="btn btn-default">Read More</button>
 																				 		<div class="row">
 																				 			<div class="col-md-4 col-sm-6 col-xs-6"><p data-price="`+data[i].price+`" class="item-price">INR `+data[i].price+`/- </p></div>
 																						 	<div class="col-md-4 col-sm-6 col-xs-6"><button data-price="`+data[i].price+`" id="buy-item" data-id="`+data[i].id+`" class="btn btn-default buy-items">Add to Cart</button></div>
@@ -145,6 +147,7 @@ $(document).ready(function() {
 																		     </div>
 																		     <div class='col-md-6 col-xs-12'>
 																		     	<p>`+data[i].description+`</p>
+																		     	<button id="load-desc" class="btn btn-default">Read More</button>
 																		     	<div class="row">
 																			 			<div class="col-md-4 col-sm-6 col-xs-6"><p class="item-price">INR `+data[i].price+`/- </p></div>
 																					 	<div class="col-md-4 col-sm-6 col-xs-6"><button data-price="`+data[i].price+`" id="buy-item" data-id="`+data[i].id+`" class="btn btn-default buy-items">Add to Cart</button></div>
@@ -219,6 +222,7 @@ $(document).ready(function() {
 																				     </div>
 																				     <div class='col-md-6 col-xs-12'>
 																				     	<p>`+data[i].description+`</p>
+																				     	<button id="load-desc" class="btn btn-default">Read More</button>
 																				     	<div class="row">
 																					 			<div class="col-md-4 col-sm-6 col-xs-6"><p class="item-price">INR `+data[i].price+`/- </p></div>
 																							 	<div class="col-md-4 col-sm-6 col-xs-6"><button data-price="`+data[i].price+`" id="buy-item" data-id="`+data[i].id+`" class="btn btn-default buy-items">Add to Cart</button></div>
